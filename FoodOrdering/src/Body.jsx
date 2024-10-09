c9e39a20a0eb0d30fc6fdf9eb04c9ff07facb114
shreyas <21b52.shreyas@sjec.ac.in>
2024-10-09 17:45:56 +0530

    loading... screen

--- FoodOrdering/src/Body.jsx
+++ FoodOrdering/src/Body.jsx
@@ -1,10 +1,24 @@
 import './Body.css';
 import restaurants from './assets/Data';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {ResCards} from './ResCard.jsx';
 
 const Body = () => {
-  const [rest, setrest] = useState(restaurants);
+  const [rest, setrest] = useState([]);
+  useEffect( ()=>{
+    fetchdata();
+  }, [])
+  const fetchdata = async () => {
+     const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+     const json = await data.json();
+     console.log(json);
+     console.log(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setrest(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  }
+  if(rest.length === 0){
+    return (
+    <h1>loading....</h1>);
+  }
   return (
     <div className="body-comp">  
    <button className='filter-btn'
